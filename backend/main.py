47ebb958a3bd2ad49198e3cd989ce450daa023e0
synguyen446 <nsn131203@gmail.com>
2026-02-28 11:55:37 -0800

    added twilio.py

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes.llm import llm_router
+from app.routes.twilio import twilio_router
 
 app = FastAPI(title="988 Crisis Chatbot API")
 
@@ -16,7 +17,7 @@ app.add_middleware(
 )
 
 app.include_router(llm_router, prefix="/llm", tags=["llm"])
-
+app.include_router(twilio_router , prefix='/twilio',tags=["twilio"])
 
 @app.get("/")
 def root():
